608bb0c6e18d75b83adeb8ed3e4c9078dff8d5ac
dlwangyilin <dlwangyilin@gmail.com>
2020-06-30 19:59:50 -0700

    Dynamic add burger ingredients

---
+++ src/Components/Burgur/Burger.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classes from './Burger.module.css';
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey}/>;
+            });
+    });
+    /*
+    Object.keys(props.ingredients) 会把这个object的所有key作为string的形式放进array里
+    比如有两层肉，那我们就需要生成两个<BurgerIngredient type = 'Meat'/>,这是通过生成一个长度为2的数组，再调用map函数实现的。
+    [...Array(props.ingredients[igKey])].map((_, i)
+     */
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type={"bread-top"} />
+            {transformedIngredients}
+            <BurgerIngredient type={"bread-bottom"} />
+        </div>
+    );
+};
+
+export default burger;
--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
 import Aux from '../../hoc/Auxiliary';
+import Burger from '../../Components/Burgur/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 1
+        }
+    };
+
     render() {
         return (
             <Aux>
-                <div> Burger </div>
+                <Burger ingredients={this.state.ingredients}/>
                 <div> Build Control </div>
             </Aux>
         );
