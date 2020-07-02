b47f787465dc3a9bc1b5bd25921d7080ba0ce349
dlwangyilin <dlwangyilin@gmail.com>
2020-07-01 17:33:54 -0700

    Add buttons on modal. Some thoughts: Handlers are passed by props and defined in the stateful components. Because they need to manipulate states. Presentational components are only in charge of interface, they don't need to handle the real logic.

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -70,6 +70,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: false})
     };
 
+    purchaseContinueHandler = () => {
+        alert("Continue");
+    };
+
     render() {
         const disableInfo = {
             ...this.state.ingredients
@@ -82,7 +86,11 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients}/>
+                    <OrderSummary
+                        ingredients={this.state.ingredients}
+                        purchaseCanceled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}
+                    />
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BurgerControls
