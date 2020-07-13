ea3e37efd2e05194689741f566fcd7d378e3e393
dlwangyilin <dlwangyilin@gmail.com>
2020-07-13 14:31:03 -0700

    Use redux-thunk to run asynv code. Redux workflow: 1. register store with reducer and middleware. 2. access state in store by `mapStateToProps` 3. manage state in store by `mapDispatchToProps` 4. within dispatch method, we need to pass an object {type: XX, payload: XX}, this object is generated by actionCreators. 5.  reducer is subscribed to specific actionTypes defined in `type` and `actionTypes.js`. 6. Before reducer, middleware is called first. We async codes here.

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -16,15 +16,11 @@ import * as burgerBuilderActions from '../../store/actions/index';
 class BurgerBuilder extends Component {
     state = {
         purchasing: false,
-        loading: false,
-        error: false
     };
 
     componentDidMount() {
-        // axios.get('/ingredients.json')
-        //     .then(response => {
-        //         this.setState({ingredients: response.data});
-        //     }).catch(error => {this.setState({error: true})});
+        console.log(this.props);
+        this.props.onInitIngredients();
     }
 
     updatePurchaseState(ingredients) {
@@ -65,7 +61,7 @@ class BurgerBuilder extends Component {
 
         let orderSummary = null;
 
-        let burger = this.state.error? <p>This can't be loaded</p> : <Spinner/>;
+        let burger = this.props.error? <p>This can't be loaded</p> : <Spinner/>;
 
         if (this.props.ings) {
             burger = (
@@ -92,9 +88,6 @@ class BurgerBuilder extends Component {
             );
         }
 
-        if (this.state.loading) {
-            orderSummary = <Spinner/>
-        }
 
         return (
             <Aux>
@@ -110,14 +103,16 @@ class BurgerBuilder extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        error: state.error
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
         onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
-        onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName))
+        onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
+        onInitIngredients: () => dispatch(burgerBuilderActions.initIngredient())
     }
 }
 
