8fefaea4b70ee4fd9cc5fbbc1196c727d0d0a3b8
dlwangyilin <dlwangyilin@gmail.com>
2020-07-17 01:52:45 -0700

    Add sign up

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,6 +24,21 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
-
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        const SignUpAPIUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=";
+        const APIKey = "AIzaSyBgYeoNKgiVA3NCDCsP0v8T-bVth0-ZDDk";
+        axios.post(SignUpAPIUrl + APIKey, authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            })
     }
 }
